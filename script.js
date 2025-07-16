58eea7de7c303d63334f52792d148fb6105b6c19
Umaid Nasir <info.umaid@gmail.com>
2025-07-16 16:11:24 -0400

    js completed and images added

--- script.js
+++ script.js
@@ -11,3 +11,64 @@ class Smoothie {
     this.ingredients = ingredients;
     this.base = base;
   }
+  serveIt() {
+    const output = document.querySelector("#output");
+
+    // Clear the output before new output
+    output.innerHTML = "";
+
+    const cup = document.createElement("img");
+
+    // Set default smoothie image
+    cup.setAttribute("src", "images/smoothie-cup.png");
+
+    // Used switch statement to adjust the image sized 
+    switch (this.size) {
+      case "small":
+        cup.setAttribute("height", "100");
+        break;
+      case "medium":
+        cup.setAttribute("height", "150");
+        break;
+      case "large":
+        cup.setAttribute("height", "200");
+        break;
+      default:
+        cup.setAttribute("height", "150"); // default size
+    }
+
+    cup.setAttribute(
+      "title",
+      `A ${this.size} smoothie with ${this.ingredients.join(", ")} and ${this.base}.`
+    );
+
+
+    output.appendChild(cup);
+
+    const description = document.createElement("p");
+    description.textContent = `You ordered a ${this.size} smoothie with ${this.ingredients.join(", ")} and a ${this.base} base.`;
+    output.appendChild(description);
+  }
+}
+
+
+document.getElementById("smoothieForm").addEventListener("submit", function (e) {
+  e.preventDefault(); // preventing the page  from refereshing
+
+  // getting the selected sized
+  let size = document.getElementById("size").value;
+
+  // getting the selected base
+  let base = document.getElementById("base").value;
+
+  //   // getting those added and checked ingredients 
+  let checkedBoxes = document.querySelectorAll('input[name="ingredients"]:checked');
+  let ingredients = [];
+  checkedBoxes.forEach(function (box) { /* loop through each check box so that to gets it value  */
+    ingredients.push(box.value);
+  });
+
+  // Smoothie Object
+  let smoothie = new Smoothie(size, ingredients, base);
+  smoothie.serveIt(); /* called the method */
+});
