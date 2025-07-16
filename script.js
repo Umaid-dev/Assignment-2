c72d648c253852f5874f8c59ec65bc0fb732338e
Umaid Nasir <info.umaid@gmail.com>
2025-07-16 16:03:24 -0400

    working on js started

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smoothie class 
+class Smoothie {
+
+    size;
+  ingredients;
+  base;
+
+  // Constructor to initialize a new Smoothie object
+  constructor(size, ingredients, base) {
+    this.size = size;
+    this.ingredients = ingredients;
+    this.base = base;
+  }
